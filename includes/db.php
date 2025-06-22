72bc1831372d9ee121d36b93afe6a9fc17243e78
IreneMaretaWidyaNingtyas <maretairen06@gmail.com>
2025-06-22 20:37:47 +0700

    i

--- includes/db.php
+++ includes/db.php
@@ -10,3 +10,17 @@ if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+
+<?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = getenv("DB_PORT") ?: 3306;
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>

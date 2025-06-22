eb17f924baed30d9abfd4082946e1acf6279a5a3
IreneMaretaWidyaNingtyas <maretairen06@gmail.com>
2025-06-22 20:52:42 +0700

    icel

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,4 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "toko_obat";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
-
-<?php
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");

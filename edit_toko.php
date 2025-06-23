c6b3733e699c9f2b118e7e9e1c92aa440e70a2db
IreneMaretaWidyaNingtyas <maretairen06@gmail.com>
2025-06-23 09:40:35 +0700

    edit_db

--- edit_toko.php
+++ edit_toko.php
@@ -1,7 +1,7 @@
 <?php
-$koneksi = mysqli_connect("hopper.proxy.rlwy.net", "root", "passwordmu", "namadb", 11750);
+$koneksi = mysqli_connect('hopper.proxy.rlwy.net', 'root', 'kgrVBYlHaoXAsSUmoXFUpLGpRvlHfkyK', 'railway', 11750);
 
-$sql = "ALTER TABLE users MODIFY COLUMN nama VARCHAR(100)";
+$sql = "ALTER TABLE users MODIFY COLUMN email VARCHAR(100)";
 if (mysqli_query($koneksi, $sql)) {
     echo "Kolom berhasil diubah.";
 } else {
